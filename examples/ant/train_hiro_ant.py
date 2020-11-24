19a6867bc1dee91c295a2c9270a979bb657aae49
MattStrong <mast4878@colorado.edu>
2020-11-23 20:19:35 -0700

    fix

--- examples/ant/train_hiro_ant.py
+++ examples/ant/train_hiro_ant.py
@@ -200,7 +200,7 @@ def main():
         GlfwContext(offscreen=True)
 
     if args.demo:
-         eval_stats = experiments.eval_performance(
+        eval_stats = experiments.eval_performance(
             env=eval_env, agent=agent, n_steps=None, n_episodes=args.eval_n_runs,
             video_outdir=args.outdir, step_number=-1 if args.recordd else None  # justNonNoneObjectForRecording
         )
